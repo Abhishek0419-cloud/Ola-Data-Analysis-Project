86fad4f013dedbe759930d5dcac642a6b3042586
Abhishek Tripathi <abhitripathi0419@gmail.com>
2026-03-28 17:51:22 +0530

    Add SQL analysis queries for ride bookings

---
+++ SQL/analysis_queries.sql
@@ -0,0 +1,34 @@
+SELECT * FROM ola.dataset;
+use ola;
+-- 1. Retrieve all successful bookings
+create view Successful_Booking as
+select * from dataset where Booking_Status = "Success";
+
+-- 2. Find the average ride distance for each vehicle type:
+select Vehicle_Type,avg(Ride_Distance) from dataset group by Vehicle_Type;
+
+-- 3. Get the total number of cancelled rides by customers
+select count(*) from dataset where Booking_Status = "Canceled by Customer";
+
+-- 4. List the top 5 customers who booked the highest number of rides
+select Customer_ID,count(Booking_ID)as Total_Bookings from dataset group by Customer_ID
+order by Total_Bookings desc limit 5;
+
+-- 5. Get the number of rides cancelled by drivers due to personal and car-related issues
+select count(*)as Personal_and_Car_related_issue from dataset where Canceled_Rides_by_Driver = "Personal & Car related issue";
+
+ -- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+select max(Driver_Ratings)as Max_Rating,min(Driver_Ratings)as Min_Rating from dataset
+where Vehicle_Type = "Prime Sedan";
+
+--  7. Retrieve all rides where payment was made using UPI:
+select * from dataset where Payment_Method = "UPI";
+
+--  8. Find the average customer rating per vehicle type:
+select Vehicle_Type,round(avg(Customer_Rating),2) from dataset group by Vehicle_Type;
+
+--  9. Calculate the total booking value of rides completed successfully:
+select sum(Booking_Value)as Total_Payment from dataset where Booking_Status = "Success";
+
+--  10. List all incomplete rides along with the reason
+select Booking_ID,Incomplete_Rides_Reason from dataset where Incomplete_Rides = "Yes";
